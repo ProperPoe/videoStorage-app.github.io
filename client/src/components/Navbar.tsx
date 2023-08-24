773c910d2daf8b50387fa04585a983f3a121407c
Potency <proper@ProperPoe>
2023-08-23 22:36:52 -0600

    Added a hamburger menu to the Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
-import {Home, Person, Notifications} from '@mui/icons-material'
+import {Menu, MenuItem, IconButton} from '@mui/material'
+import {Home, Person, Notifications, Search, Menu as MenuIcon} from '@mui/icons-material'
 
 interface Props {}
 interface State {}
@@ -19,8 +20,22 @@ class Navbar extends PureComponent<Props, State> {
             <>
             <div className='bg-gray-800 py-4'>
                 <div className='container mx-auto flex justify-between items-center'>
-                    {/* Logo */}
-                    <div className='text-white text-xl font-bold'>Logo</div>
+                    <div className='flex items-center'>
+                        <IconButton
+                            sx={{color: 'white'}}
+                            color='inherit'
+                            edge='start'
+                            aria-label='menu'
+                            aria-controls='menu'
+                            aria-haspopup="true"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        {/* Logo */}
+                        <div className='text-white text-xl font-bold'>Logo</div>
+
+                    </div>
+                    {/* Hamburger Icon */}
 
                     {/* Navigation Links */}
                     <div className='text-white space-x-4'>
@@ -32,7 +47,7 @@ class Navbar extends PureComponent<Props, State> {
                      {/* Seach Input */}
                      <div className='flex items-center'>
                         <input type="text" placeholder='Search' className='rounded-l-lg px-3 py-2 bg-gray-700 text-gray-200 focus:outline-none' />
-                        <button className='rounded-r-lg px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 transition duration-300'>Search</button>
+                        <button className='rounded-r-lg px-4 py-2 bg-blue-500 text-white hover:bg-blue-600 transition duration-300'><Search /></button>
                      </div>
                 </div>
             </div>
