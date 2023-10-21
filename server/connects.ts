28c28f09e9683fff159fc09ca55978882f40da6e
Potency <proper@ProperPoe>
2023-10-20 23:03:22 -0600

    fix

--- server/connects.ts
+++ server/connects.ts
@@ -1,11 +1,11 @@
-import mysql from "mysql2";
+import mysql2 from "mysql2";
 import dotenv from "dotenv"
 
 dotenv.config();
 
 const port = process.env.DB_PRODUCTION_PORT ? parseInt(process.env.DB_PRODUCTION_PORT) : 3306;
 
-export const db = mysql.createConnection({
+export const db = mysql2.createConnection({
     host: process.env.DB_PRODUCTION_HOST,
     user: process.env.DB_PRODUCTION_USER,
     password: process.env.DB_PRODUCTION_PASSWORD,
